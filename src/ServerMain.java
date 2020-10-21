c6bfea56651878ec0982ac39b42ac2188b1e95ee
Diogo Sequeira <diogosequeira@hotmail.fr>
2020-10-21 10:49:11 +0100

    Update ServerMain.java

--- src/ServerMain.java
+++ src/ServerMain.java
@@ -1,11 +1,8 @@
 public class ServerMain {
 
     public static void main(String[] args) {
-
         int port = 8080;
         Servidor server = new Servidor(port);
         server.start();
-
     }
-
 }
